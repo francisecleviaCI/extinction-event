17b1d98f9048710565922bc8656acdad60106567
Francis Eclevia <francis.eclevia@codeimmersives.com>
2020-08-05 03:07:19 -0400

    hw

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//--#1--//
 const lineThroughText = function(event) {
     const clickedItem = event.target
     clickedItem.style.textDecoration = 'line-through';
@@ -8,6 +9,8 @@ const lineThroughText = function(event) {
     item.addEventListener('click', lineThroughText);
   }
 
+
+//--#2--//
   const giveMeOpacity = function(event) {
     const clickedItem = event.target
     clickedItem.style.opacity= '0';
@@ -17,3 +20,36 @@ const lineThroughText = function(event) {
   for (const item of allItems2) {
     item.addEventListener('click', giveMeOpacity);
   }
+
+//--#3--//
+const adiosDinosaur = function(event) {
+    const clickedItem = event.target
+    clickedItem.style.opacity= '0';
+  }
+  
+  const allItems3 = document.querySelectorAll('section');
+  for (const item of allItems3) {
+    item.addEventListener('click', adiosDinosaur);
+  }
+
+//--#4--//
+
+
+const totalExtinction = function() {
+    allItems.style.textDecoration = 'line-through'; 
+    allItems2.style.opacity= '0'; 
+    allItems3.style.opacity= '0'; 
+  }
+  
+  const allItems4 = document.querySelector('#destroy-all');
+  for (const item of allItems) {
+    item.addEventListener('click', totalExtinction);
+  } 
+  for (const item of allItems2) {
+        item.addEventListener('click', totalExtinction);
+    } 
+    for (const item of allItems3) {
+            item.addEventListener('click', totalExtinction);
+  }
+
+    
